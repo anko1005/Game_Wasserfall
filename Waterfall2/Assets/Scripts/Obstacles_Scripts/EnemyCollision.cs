de1f51433d5752c945cda99fb8c2febf00f22080
SemiraEmely <semira.rainer@outlook.de>
2022-07-10 14:18:18 +0200

    Update EnemyCollision.cs
    destroy gameobject when hit

--- Waterfall2/Assets/Scripts/Obstacles_Scripts/EnemyCollision.cs
+++ Waterfall2/Assets/Scripts/Obstacles_Scripts/EnemyCollision.cs
@@ -14,6 +14,7 @@ public class EnemyCollision : MonoBehaviour
             {
                 notHit = false;
                 collision.gameObject.GetComponent<PlayerMove>().ObstacleHit();
+                Destroy(this.gameObject);
             }
         }
 
